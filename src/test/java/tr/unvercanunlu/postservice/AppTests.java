dc9cb5f78677ae3fcfd6654b1386461de6862bad
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-03-02 14:07:34 +0100

    app test is added.

---
+++ src/test/java/tr/unvercanunlu/postservice/AppTests.java
@@ -0,0 +1,13 @@
+package tr.unvercanunlu.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

39efc2666360e20f17195870f303bbb7c040cc30
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-03-03 11:11:06 +0100

    api gateway and service discovery are added.

---
+++ api-gateway/src/main/java/tr/unvercanunlu/microservices/apigateway/RouteConfig.java
@@ -0,0 +1,18 @@
+package tr.unvercanunlu.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(route -> route.path("/api/v1/posts/**")
+                        .uri("lb://POST-SERVICE/"))
+                .build();
+    }
+}

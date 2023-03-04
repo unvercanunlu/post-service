0028325824ccb8b430cc1f74bd786a22da39bf42
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-03-04 22:41:18 +0100

    unnecessary throws is removed.

--- post-service/src/main/java/tr/unvercanunlu/microservices/postservice/consumer/IMessageConsumer.java
+++ post-service/src/main/java/tr/unvercanunlu/microservices/postservice/consumer/IMessageConsumer.java
@@ -1,9 +1,8 @@
 package tr.unvercanunlu.microservices.postservice.consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public interface IMessageConsumer<K, V> {
 
-    void onMessage(ConsumerRecord<K, V> payload) throws JsonProcessingException;
+    void onMessage(ConsumerRecord<K, V> payload);
 }
